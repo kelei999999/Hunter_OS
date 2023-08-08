ab47e364f83d4d831c5985076164c281c3ad6f5c
kelei999999 <103822551+kelei999999@users.noreply.github.com>
2023-08-08 18:07:20 +0800

    Add files via upload

--- Typedef.h
+++ Typedef.h
@@ -1,27 +1,27 @@
-/* Kelei999999(WangLiang) all rights reserved.  You may use this software
- * and any derivatives exclusively with Kelei999999(WangLiang) products.
+/* Hunter.ORG all rights reserved.  You may use this software
+ * and any derivatives exclusively with Hunter.ORG products.
  *
- * THIS SOFTWARE IS SUPPLIED BY Kelei999999(WangLiang) "AS IS".  NO WARRANTIES, WHETHER
+ * THIS SOFTWARE IS SUPPLIED BY Hunter.ORG "AS IS".  NO WARRANTIES, WHETHER
  * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
  * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
- * PARTICULAR PURPOSE, OR ITS INTERACTION WITH Kelei999999(WangLiang) PRODUCTS, COMBINATION
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH Hunter.ORG PRODUCTS, COMBINATION
  * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
  *
- * IN NO EVENT WILL Kelei999999(WangLiang) BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+ * IN NO EVENT WILL Hunter.ORG BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
  * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
- * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF Kelei999999(WangLiang) HAS
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF Hunter.ORG HAS
  * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE
- * FULLEST EXTENT ALLOWED BY LAW, Kelei999999(WangLiang)'S TOTAL LIABILITY ON ALL CLAIMS
+ * FULLEST EXTENT ALLOWED BY LAW, Hunter.ORG'S TOTAL LIABILITY ON ALL CLAIMS
  * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF
- * ANY, THAT YOU HAVE PAID DIRECTLY TO Kelei999999(WangLiang) FOR THIS SOFTWARE.
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO Hunter.ORG FOR THIS SOFTWARE.
  *
- * Kelei999999(WangLiang) PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+ * Hunter.ORG PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
  * TERMS.
  */
 
 /*
  * File: Typedef.h
- * Author: Kelei999999(WangLiang)
+ * Author: Hunter.ORG
  * Created on: 2016年2月16日
  *
  * -Modified for F28069 DSP data types for int16 and int32 Joe Zhou 01-03-2018
@@ -37,17 +37,6 @@
 
 #ifndef TYPEDEF_H_
 #define TYPEDEF_H_
-
-#define uint8_T				unsigned char
-#define int8_T				char
-#define uint16_T			unsigned short
-#define int16_T				short
-#define uint32_T			unsigned long
-#define int32_T				int
-#define uint64_T			unsigned long long
-#define int64_T				long long
-#define real32_T			float
-#define real64_T			double
 
 typedef unsigned char		uint8;		/*无符号8位*/
 typedef char				int8;		/*有符号8位*/
